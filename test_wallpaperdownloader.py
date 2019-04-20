dcdc848e47cf0fc04cdf677b514480fc05daf7f2
Konstantin V <mail@k-vinogradov.ru>
2019-04-20 18:36:04 +0700

    Updated test

--- test_wallpaperdownloader.py
+++ test_wallpaperdownloader.py
@@ -10,9 +10,11 @@ from wallpaperdownloader import download
     "args", [(1, 2017, "800x600", 30), (5, 2018, "1920x1080", 35)]
 )
 def test_full_sequence(tmpdir, args):
+    """Test if images were successfully downloaded."""
     month, year, image_size, expected_number_of_files = args
     os.chdir(tmpdir)
     asyncio.run(download(year, month, image_size))
     file_list = os.listdir(tmpdir)
     assert len(file_list) == expected_number_of_files, "Invalid file count"
-
+    for filepath in map(lambda s: os.path.join(tmpdir, s), file_list):
+        assert os.path.getsize(filepath) > 0, "Invalid file size"
